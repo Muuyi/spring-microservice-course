806c39b89802f3fe4bcc200b1aef329c4b9e4480
Muuyi Andrew <muuyiandrew2015@gmail.com>
2023-11-30 07:42:20 +0300

    Initial

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,21 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	
+	public List<User> findAll(){
+		return null;
+		//return users;
+	}
+	
+	public User findOne(int id) {
+		return null;
+	}
+	public User save(User user) {
+		return null;
+	}
+}
